df17cdba8f437bee46407baad31c4444a1a383eb
Benedettopana <benedetto.pana@gmail.com>
2024-03-20 14:27:07 +0100

    [add]title/custom-prop

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -1,30 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+};
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>{{ title }}</h1>
+    <p class="my-p-color">
+      morning joined movement receive in sense drink river entirely said whale
+      result finish never white diameter able own example feed usually mark
+      breeze personal
+    </p>
+    <div class="my-custom-prop">
+      <p>
+        younger paragraph wash his gold cat magic function personal tried
+        passage care diameter studied addition me refer my golden usually thirty
+        shirt pool store
+      </p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+div {
+  text-align: center;
+  background: lightgray;
+  padding: 5px 10px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+h1 {
+  color: aliceblue;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+p.my-p-color {
+  background-color: red;
+  color: white;
+  padding: 5px 10px;
+  margin: 10px 20px;
+}
+
+.my-custom-prop {
+  background-color: blue;
+  color: black;
 }
 </style>
